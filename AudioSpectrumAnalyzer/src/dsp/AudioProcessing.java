83502d4665ec811728f93ff4bce475443a2586de
zz4fap <zz4fap@84c98125-e114-56cc-d498-39303f8bdc64>
2012-01-31 20:12:33 +0000

    changes in order to improve UI

--- AudioSpectrumAnalyzer/src/dsp/AudioProcessing.java
+++ AudioSpectrumAnalyzer/src/dsp/AudioProcessing.java
@@ -1,6 +1,6 @@
 package dsp;
 
-import dsp.SignalHelper.Signal;
+import dsp.SignalHelper.SignalGenerator;
 import fft.Constants;
 import fft.FFTHelper;
 import android.media.AudioFormat;
@@ -12,10 +12,11 @@ public class AudioProcessing extends Thread {
 	
 	private static final String TAG = AudioProcessing.class.getSimpleName();
 	
-	private int mSampleRateInHz = (int)Constants.SAMPLING_FREQUENCY;
+	private static double mSampleRateInHz = Constants.SAMPLING_FREQUENCY;
+	private static int mNumberOfFFTPoints = Constants.NUMBER_OF_FFT_POINTS;
+	
 	private AudioRecord mRecorder = null;
 	private int mMinBufferSize;
-	private double mSignal[];
 	
 	private boolean stopped = false;
 	
@@ -26,13 +27,7 @@ public class AudioProcessing extends Thread {
 	private int mDrawableSignal[];
 	
 	public AudioProcessing(){
-		mSampleRateInHz = (int)Constants.SAMPLING_FREQUENCY;
 		mFFT = new FFTHelper();
-		start();
-	}
-	
-	public AudioProcessing(int sampleRateInHz){
-		mSampleRateInHz = sampleRateInHz;
 		start();
 	}
 	
@@ -47,23 +42,24 @@ public class AudioProcessing extends Thread {
 	
 	private void runWithSignalHelper(){ // TESTE
 		int numberOfReadBytes = 0;
+		double[] absNormalizedSignal;
 		
-		Signal signal = new Signal();
-		signal.setFrequency(1000);
+		SignalGenerator signalGenerator = new SignalGenerator();
 
 		while(!stopped) {
 			byte tempBuffer[] = new byte[Constants.BUFFER_SIZE]; // 2*Buffer size because it's a short variable into a array of bytes.
-			mSignal = new double[Constants.BUFFER_SIZE/2];
-			//numberOfReadBytes = signal.read(tempBuffer,0,Constants.BUFFER_SIZE,2);
-			numberOfReadBytes = signal.read(tempBuffer,2);
+			double[] signal = new double[Constants.BUFFER_SIZE/2];
+			numberOfReadBytes = signalGenerator.read(tempBuffer,500,getSampleRateInHz(),true,false);
 			if(numberOfReadBytes > 0){
 				for(int i = 0; i < Constants.BUFFER_SIZE/2; i++){
-					mSignal[i] = (double)((tempBuffer[2*i] & 0xFF) | (tempBuffer[2*i+1] << 8)) / 32768.0F;
+					signal[i] = (double)((tempBuffer[2*i] & 0xFF) | (tempBuffer[2*i+1] << 8)) / 32768.0F;
 				}
-				// Calculate captured signal's FFT.
-				mFFT.calculateFFT(mSignal); 
-				mDrawableSignal = SignalHelper.getDrawableFFTSignal(mFFT.getAbsFFTSignal());
-				notifyListenersOnFFTSamplesAvailableForDrawing();
+				if(mFFT!=null){
+					// Calculate captured signal's FFT.
+					absNormalizedSignal = mFFT.calculateFFT(signal); 
+					mDrawableSignal = SignalHelper.getDrawableFFTSignal(absNormalizedSignal);
+					notifyListenersOnFFTSamplesAvailableForDrawing();
+				}
 			} else {
 				Log.e(TAG,"There was an error reading the audio device - ERROR: "+numberOfReadBytes);
 			}
@@ -72,10 +68,11 @@ public class AudioProcessing extends Thread {
 	
 	private void runWithAudioRecord(){
 		int numberOfReadBytes = 0;
+		double[] absNormalizedSignal;
 		
-		mMinBufferSize = AudioRecord.getMinBufferSize(mSampleRateInHz,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
+		mMinBufferSize = AudioRecord.getMinBufferSize((int)mSampleRateInHz,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
 		mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-				mSampleRateInHz, AudioFormat.CHANNEL_IN_MONO,
+				(int)mSampleRateInHz, AudioFormat.CHANNEL_IN_MONO,
 				AudioFormat.ENCODING_PCM_16BIT, 10*mMinBufferSize);
 		
 		if(mRecorder==null)
@@ -85,16 +82,18 @@ public class AudioProcessing extends Thread {
 
 		while(!stopped) {
 			byte tempBuffer[] = new byte[Constants.BUFFER_SIZE];
-			mSignal = new double[Constants.BUFFER_SIZE/2];
+			double[] signal = new double[Constants.BUFFER_SIZE/2];
 			numberOfReadBytes = mRecorder.read(tempBuffer,0,Constants.BUFFER_SIZE);
 			if(numberOfReadBytes > 0){
 				for(int i = 0; i < Constants.BUFFER_SIZE/2; i++){
-					mSignal[i] = (double)((tempBuffer[2*i] & 0xFF) | (tempBuffer[2*i+1] << 8)) / 32768.0F;
+					signal[i] = (double)((tempBuffer[2*i] & 0xFF) | (tempBuffer[2*i+1] << 8)) / 32768.0F;
 				}
-				// Calculate captured signal's FFT.
-				mFFT.calculateFFT(mSignal); 
-				mDrawableSignal = SignalHelper.getDrawableFFTSignal(mFFT.getAbsFFTSignal());
-				notifyListenersOnFFTSamplesAvailableForDrawing();
+				if(mFFT!=null){
+					// Calculate captured signal's FFT.
+					absNormalizedSignal = mFFT.calculateFFT(signal); 
+					mDrawableSignal = SignalHelper.getDrawableFFTSignal(absNormalizedSignal);
+					notifyListenersOnFFTSamplesAvailableForDrawing();
+				}
 			} else {
 				Log.e(TAG,"There was an error reading the audio device - ERROR: "+numberOfReadBytes);
 			}
@@ -104,8 +103,20 @@ public class AudioProcessing extends Thread {
         mRecorder.release();
 	}
 	
-	public int getSampleRateInHz(){
+	public static double getSampleRateInHz(){
 		return mSampleRateInHz;
+	}
+	
+	public void setSampleRateInHz(double fs){
+		mSampleRateInHz = fs;
+	}
+	
+	public static int getNumberOfFFTPoints(){
+		return mNumberOfFFTPoints;
+	}
+	
+	public void setNumberOfFFTPoints(int fftPoints){
+		mNumberOfFFTPoints = fftPoints;
 	}
 	
 	public void close(){ 
