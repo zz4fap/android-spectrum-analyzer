cce534dc5f66d52a9bb4a5b4346dd022d17f8adb
zz4fap <zz4fap@84c98125-e114-56cc-d498-39303f8bdc64>
2012-02-02 02:28:50 +0000

    Get back my own identation

--- trunk/AudioSpectrumAnalyzer/src/fft/Constants.java
+++ trunk/AudioSpectrumAnalyzer/src/fft/Constants.java
@@ -2,8 +2,8 @@ package fft;
 
 public class Constants {
 
-    // public static final double SAMPLING_FREQUENCY = 44100.0;
     public static final double SAMPLING_FREQUENCY = 8000.0;
+    
     public static final int NUMBER_OF_FFT_POINTS = 512;
 
     // 2*Fs because it's a short variable into a array of bytes.
