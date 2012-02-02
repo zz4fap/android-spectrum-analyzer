d2cd4a8f2d466671d156a6f46750312fa7e4b6a6
zz4fap <zz4fap@84c98125-e114-56cc-d498-39303f8bdc64>
2012-02-02 03:27:53 +0000

    Creating a private branch for user 2

---
+++ branches/user2/AudioSpectrumAnalyzer/src/fft/Constants.java
@@ -0,0 +1,16 @@
+package fft;
+
+public class Constants {
+
+    public static final double SAMPLING_FREQUENCY = 8000.0;
+    
+    public static final int NUMBER_OF_FFT_POINTS = 512;
+
+    // 2*Fs because it's a short variable into a array of bytes.
+    //Samples - it will always be equal to 1[s] stored into an array of shorts.
+    public static final int BUFFER_SIZE = (int) (2 * SAMPLING_FREQUENCY);
+
+    public final static double PI = 4 * Math.atan((double) 1);
+
+    public final static boolean DEBUG_MODE = true;
+}
