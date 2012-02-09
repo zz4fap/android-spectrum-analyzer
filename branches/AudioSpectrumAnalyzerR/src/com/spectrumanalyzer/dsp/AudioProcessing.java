2617f68a9d459a27abedab0ba456072fa75e08d7
zz4fap <zz4fap@84c98125-e114-56cc-d498-39303f8bdc64>
2012-02-09 22:05:02 +0000

    improvement on FFT's calculation efficiency

--- branches/AudioSpectrumAnalyzerR/src/com/spectrumanalyzer/dsp/AudioProcessing.java
+++ branches/AudioSpectrumAnalyzerR/src/com/spectrumanalyzer/dsp/AudioProcessing.java
@@ -7,6 +7,7 @@ import com.spectrumanalyzer.dsp.SignalHelper.DebugSignal;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.util.Log;
 
 public class AudioProcessing extends Thread {
 	
@@ -49,17 +50,17 @@ public class AudioProcessing extends Thread {
 		}
 	}
 	
-	private void runWithSignalHelper() { // DEBUG_MODE
+	private void runWithSignalHelper() { // DEBUG_MODE - simulated sinusoids signal
 		int numberOfReadBytes = 0, bufferSize = 2*mNumberOfFFTPoints;
 		double[] absNormalizedSignal;
+		byte tempBuffer[] = new byte[bufferSize]; // 2*Buffer size because it's a short variable into a array of bytes.
 
 		while(!mStopped) {
-			byte tempBuffer[] = new byte[bufferSize]; // 2*Buffer size because it's a short variable into a array of bytes.
 			numberOfReadBytes = DebugSignal.read(tempBuffer,mNumberOfFFTPoints,mSampleRateInHz);
 			if(numberOfReadBytes > 0) {
 				if(mFFT!=null) {
 					// Calculate captured signal's FFT.
-					absNormalizedSignal = mFFT.calculateFFT(tempBuffer);
+					absNormalizedSignal = mFFT.calculateFFT(tempBuffer, numberOfReadBytes);
 					notifyListenersOnFFTSamplesAvailableForDrawing(absNormalizedSignal);
 				}
 			} else {
@@ -71,6 +72,7 @@ public class AudioProcessing extends Thread {
 	private void runWithAudioRecord() { // REAL_MODE - BUILT IN AUDIO DEVICE
 		int numberOfReadBytes = 0, bufferSize = 2*mNumberOfFFTPoints;
 		double[] absNormalizedSignal;
+		byte tempBuffer[] = new byte[bufferSize];
 		
 		mMinBufferSize = AudioRecord.getMinBufferSize((int)mSampleRateInHz,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
 		mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
@@ -84,12 +86,11 @@ public class AudioProcessing extends Thread {
 		mRecorder.startRecording();
 
 		while(!mStopped) {
-			byte tempBuffer[] = new byte[bufferSize];
 			numberOfReadBytes = mRecorder.read(tempBuffer,0,bufferSize);
 			if(numberOfReadBytes > 0) {
 				if(mFFT!=null) {
 					// Calculate captured signal's FFT.
-					absNormalizedSignal = mFFT.calculateFFT(tempBuffer);
+					absNormalizedSignal = mFFT.calculateFFT(tempBuffer, numberOfReadBytes);
 					notifyListenersOnFFTSamplesAvailableForDrawing(absNormalizedSignal);
 				}
 			} else {
