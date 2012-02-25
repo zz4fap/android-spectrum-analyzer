899afd0e313e066fd2f8cf3871b7b404cd25be90
zz4fap <zz4fap@84c98125-e114-56cc-d498-39303f8bdc64>
2012-02-25 04:15:58 +0000

    Change data member's names, Add conversion methods and use them

--- branches/AudioSpectrumAnalyzerR/src/com/spectrumanalyzer/ui/Panel.java
+++ branches/AudioSpectrumAnalyzerR/src/com/spectrumanalyzer/ui/Panel.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.Display;
@@ -12,12 +13,11 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 
 public class Panel extends SurfaceView implements SurfaceHolder.Callback {
-
+	
 	private int mWidth; 
 	private int mHeight;
-	private int mOrientation;
 	private Display mDisplay;
-	private boolean isSurfaceCreated;
+	private boolean mIsSurfaceCreated;
 	private SurfaceHolder mSurfaceHolder;
 
 	private static final int MARK_SIZE = 7;
@@ -41,7 +41,7 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 	}
 	
 	public void drawSpectrum(double[] absSignal, double samplingRate, int numberOfFFTPoints, double maxFFTSample, int markFreqPos, int drawableArea, int pointToStartDrawing) {
-    	if(isSurfaceCreated){
+    	if(mIsSurfaceCreated){
     		Canvas canvas;
     		canvas = null;
     		try {
@@ -65,7 +65,7 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 	}
 	
 	public void drawEmptySpectrum(double samplingRate, int markFreqPos, int drawableArea, int pointToStartDrawing) {
-    	if(isSurfaceCreated){
+    	if(mIsSurfaceCreated){
     		Canvas canvas;
     		canvas = null;
     		try {
@@ -90,8 +90,9 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 		Paint p = new Paint();
 
 		for(int freq = freqStep; freq <= (int)(samplingRate/2); freq = freq + freqStep) {
-			double point = (double)(((double)drawableArea/2)*freq)/((samplingRate/4));
-			int pointInt = (int)point;
+			
+			// convert from frequency to pixel position
+			int pointInt = convertFromFrequencyToPixel((double)freq, samplingRate, drawableArea);
 
 			p.setColor(Color.GREEN);
 			p.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
@@ -113,14 +114,15 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 	
 	private void drawFFTSignal(Canvas canvas, double[] absSignal, int numberOfFFTPoints, double maxFFTSample, int drawableArea, int pointToStartDrawing) {
 		int sampleValue;
-		double pos;
+		int pos;
 		Paint p = new Paint();
 		p.setColor(Color.WHITE);
-		for(int i = 0; i < (absSignal.length-1); i++) {
-			sampleValue = (int)(SCALE_FACTOR*(absSignal[i]/maxFFTSample));			
-			pos = (double)(((double)(((double)drawableArea/2)*i))/(double)(numberOfFFTPoints/4));
-			if(((int)pos) >= pointToStartDrawing) {
-				canvas.drawLine(((int)pos-pointToStartDrawing),(mHeight-2),((int)pos-pointToStartDrawing),((mHeight-2)-sampleValue), p);
+		for(int i = 0; i < absSignal.length; i++) {
+			sampleValue = (int)(SCALE_FACTOR*(absSignal[i]/maxFFTSample));
+			// convert from fft sample to pixel position
+			pos = convertFromFFTSampleToPixel(i, numberOfFFTPoints, drawableArea);
+			if(pos >= pointToStartDrawing) {
+				canvas.drawLine((pos-pointToStartDrawing),(mHeight-2),(pos-pointToStartDrawing),((mHeight-2)-sampleValue), p);
 			}
 		}
 	}
@@ -128,9 +130,10 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 	private void drawMarkFrequencyAndText(Canvas canvas, int markPixelPos, double samplingRate, int drawableArea, int pointToStartDrawing) {
 		Paint p = new Paint();
 		p.setColor(Color.WHITE);
-		p.setTextSize(12);
+		p.setTextSize(17);
 		p.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-		double markFreq = (double)((double)(markPixelPos*(samplingRate/4))/(double)(drawableArea/2));
+		//convert from pixel position to frequency.
+		double markFreq = convertFromPixelToFrequency(markPixelPos, samplingRate, drawableArea);		
 		canvas.drawText("Mark Freq: "+markFreq+" Hz",(mWidth/2),(mHeight-165),p);	
 		p.setColor(Color.GREEN);
 		if(markPixelPos >= pointToStartDrawing) {
@@ -146,7 +149,37 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 		canvas.drawLine((mWidth-1),0,(mWidth-1),(mHeight-1),p);
 		canvas.drawLine(0,(mHeight-1),(mWidth-1),(mHeight-1),p);
 	}
+	
+	// Conversion from pixel point to FFT Sample
+	public static int convertFromPixelToFFTSample(int pixel, int numberOfFFTPoints, int drawableArea) {
+		double sample = Math.round((double)pixel*((numberOfFFTPoints/2)-1)/(double)(drawableArea-1));
+		return (int)sample;	
+	}
 
+	// Conversion from frequency to pixel point
+	public static int convertFromFrequencyToPixel(double frequency, double samplingRate, int drawableArea) {
+		double pixel = Math.round((frequency*(drawableArea-1))/(samplingRate/2));
+		return (int)pixel;
+	}
+
+	// Conversion from pixel point to frequency
+	public static double convertFromPixelToFrequency(int pixel, double samplingRate, int drawableArea) {
+		double freq = Math.round(pixel*(double)((samplingRate/2)/(drawableArea-1)));
+		return freq;
+	}
+
+	// Conversion from FFT sample to pixel position
+	public static int convertFromFFTSampleToPixel(int sample, int numberOfFFTPoints, int drawableArea) {
+		double pixel = Math.round(sample*((double)(drawableArea-1)/((numberOfFFTPoints/2)-1)));
+		return (int)pixel;
+	}
+	
+	// Conversion from FFT Sample to frequency, note that it's an approximation of the real frequency value.
+	public static double convertFromFFTSampleToFrequency(int sample, int numberOfFFTPoints, double samplingRate, int drawableArea) {
+		int pixel = convertFromFFTSampleToPixel(sample, numberOfFFTPoints, drawableArea);
+		return convertFromPixelToFrequency(pixel, samplingRate, drawableArea);
+	}
+	
 	private void drawPeakFrequencyMarkAndText(Canvas canvas, double peakFreq) {
 		Paint p = new Paint();
 		p.setColor(Color.WHITE);
@@ -166,22 +199,26 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
 	private void getViewInfo() {
 		mWidth = getWidth();
 		mHeight = getHeight();
-		mOrientation = mDisplay.getOrientation();
+	}
+	
+	public int getPanelWidth() {
+    	Point outSize = new Point();
+    	mDisplay.getSize(outSize);
+    	return outSize.x;
 	}
 
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
 		mWidth = width;
 		mHeight = height;
-		mOrientation = mDisplay.getOrientation();
 	}
 	
 	public void surfaceCreated(SurfaceHolder holder) {
-		isSurfaceCreated = true;
+		mIsSurfaceCreated = true;
 		getViewInfo();
 	}
 	
 	public void surfaceDestroyed(SurfaceHolder holder) {
-		isSurfaceCreated = false;
+		mIsSurfaceCreated = false;
 	}
 }   
