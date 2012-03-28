905c712d8d837ac5bed071d7adc92af0190d4d64
zz4fap <zz4fap@84c98125-e114-56cc-d498-39303f8bdc64>
2012-03-28 13:52:56 +0000

    Fix problems with JNI C++ code

--- branches/AudioSpectrumAnalyzerR/jni/Fft.hh
+++ branches/AudioSpectrumAnalyzerR/jni/Fft.hh
@@ -17,6 +17,13 @@
 #ifndef LEGACY_TALKIN_FFT_H
 #define LEGACY_TALKIN_FFT_H
 
+#include <android/log.h>
+
+#define  LOG_TAG    "native-fft-jni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
 class Fft {
 public:
     Fft(void);
--- branches/AudioSpectrumAnalyzerR/jni/NativeFFT.cpp
+++ branches/AudioSpectrumAnalyzerR/jni/NativeFFT.cpp
@@ -3,11 +3,15 @@
 
 #include <jni.h>
 
-//using namespace android;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FFT_VERSION 1
 
 static FFTNativeHelper *mFFTNativeHelper = NULL;
 
-jboolean Java_com_spectrumanalyzer_fft_NativeFFTHelper_initFft(JNIEnv* env, jclass clazz, jdouble sampleRate, jint fftSize) {
+JNIEXPORT jboolean JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_initFft(JNIEnv* env, jclass clazz, jdouble sampleRate, jint fftSize) {
 
 	mFFTNativeHelper = new FFTNativeHelper(sampleRate, fftSize);
 	if(mFFTNativeHelper)
@@ -16,22 +20,65 @@ jboolean Java_com_spectrumanalyzer_fft_NativeFFTHelper_initFft(JNIEnv* env, jcla
 		return false;
 }
 
-jdoubleArray Java_com_spectrumanalyzer_fft_NativeFFTHelper_calculateFFT(JNIEnv* env, jclass clazz, jbyteArray jSignal, jint numberOfReadBytes) {
+JNIEXPORT jdoubleArray JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_calculateFFT(JNIEnv* env, jclass clazz, jbyteArray jSignal, jint numberOfReadBytes) {
 
 	jdouble *mAbsSignal = NULL;
 	jdoubleArray output = NULL;
+	jboolean isCopy1, isCopy2;
 
 	if(mFFTNativeHelper) {
-		jbyte* buffer = env->GetByteArrayElements(jSignal, NULL);
+		jbyte* buffer = env->GetByteArrayElements(jSignal, &isCopy1);
 
 		mAbsSignal = mFFTNativeHelper->calculateFFT((char*)buffer, numberOfReadBytes);
-		env->ReleaseByteArrayElements(jSignal, buffer, 0);
 
 		//Copy mAbsSignal to a double array
 		int size = mFFTNativeHelper->getFFTSize()/2;
 		output = env->NewDoubleArray(size);
-		env->SetDoubleArrayRegion(output, 0, size, mAbsSignal);
+
+	    jdouble* destArrayElems = env->GetDoubleArrayElements(output, &isCopy2);
+
+	    for (int i = 0; i < size; i++) {
+	       destArrayElems[i] = mAbsSignal[i];
+	    }
+
+	    if (isCopy1 == JNI_TRUE) {
+	    	env->ReleaseByteArrayElements(jSignal, buffer, 0);
+	    }
+
+	    if (isCopy2 == JNI_TRUE) {
+	    	env->ReleaseDoubleArrayElements(output, destArrayElems, 0);
+	    }
+
+		//env->SetDoubleArrayRegion(output, 0, size, mAbsSignal);
 	}
 
 	return output;
 }
+
+JNIEXPORT jint JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_getNativeFftVersion(JNIEnv* env, jclass clazz) {
+	return FFT_VERSION;
+}
+
+JNIEXPORT jdouble JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_getPeakFrequency(JNIEnv* env, jclass clazz) {
+	return mFFTNativeHelper->getPeakFrequency();
+}
+
+JNIEXPORT jint JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_getPeakFrequencyPosition(JNIEnv* env, jclass clazz) {
+	return mFFTNativeHelper->getPeakFrequencyPosition();
+}
+
+JNIEXPORT jdouble JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_getMaxFFTSample(JNIEnv* env, jclass clazz) {
+	return mFFTNativeHelper->getMaxFFTSample();
+}
+
+JNIEXPORT void JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_setSamplingRate(JNIEnv* env, jclass clazz, jdouble sampleRate) {
+	return mFFTNativeHelper->setSamplingRate(sampleRate);
+}
+
+JNIEXPORT void JNICALL Java_com_spectrumanalyzer_fft_NativeFFTHelper_setNumberOfFFTPoints(JNIEnv* env, jclass clazz, jint numberOfFFTPoints) {
+	return mFFTNativeHelper->setNumberOfFFTPoints(numberOfFFTPoints);
+}
+
+#ifdef __cplusplus
+}
+#endif
